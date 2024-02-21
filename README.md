389fae3a2710c4dc20413957349ca648cc01ae5b
Fahryandi Herlasmara Putra <fahryandi.herlasmara@gmail.com>
2024-02-21 10:21:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-webpack-template-repository
+
+A template repository for web development using javascript and webpack.
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  resolve: {
+    alias: {
+      Templates: path.resolve(__dirname, "src/templates/"),
+    },
+  },
+};
